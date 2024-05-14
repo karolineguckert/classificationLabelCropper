37497b7615da1213b61379607c4be8c167bf1d18
karolineguckert <karolinesouzaguckert@gmail.com>
2024-05-14 02:09:32 -0300

    adjusting methods

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 
-from src.cropper.ClassificationLabelCropper import ClassificationLabelCropper
-from src.organize_images.OrganizeImagesToYolo import OrganizeImagesToYolo
+from src.image_cropper.ImageCropper import ImageCropper
 
 if __name__ == '__main__':
     # ClassificationLabelCropper('images').create_crops_from_all_images()
@@ -10,9 +9,11 @@ if __name__ == '__main__':
 
     # ClassificationLabelCropper().get_images_sizes()
     # OrganizeImagesToYolo().remove_images_of_val_in_folder_train()
-    classificationLabelCropper = ClassificationLabelCropper('images_without_class')
+    # classificationLabelCropper = ClassificationLabelCropper('images_without_class')
     # classificationLabelCropper.create_crops_from_all_images()
-    classificationLabelCropper.copy_images_cropped_to_folder_all_images()
+    # classificationLabelCropper.copy_images_cropped_to_folder_all_images()
 
-    classificationLabelCropper.copy_images_cropped_to_folder_all_images_by_type()
+    # classificationLabelCropper.copy_images_cropped_to_folder_all_images_by_type()
 
+    # OrganizeImagesToYolo().remove_images_of_val_in_folder_train()
+    ImageCropper().teste()
